b48e81c7fde3b4e849852521a05d60dcab0addf8
awr417h <abdullahwaqar8@gmail.com>
2020-01-07 17:18:08 +0500

    getting somewhere

--- app/src/main/java/com/example/qrattendancesystem/ui/SplashScreen.kt
+++ app/src/main/java/com/example/qrattendancesystem/ui/SplashScreen.kt
@@ -1,7 +1,10 @@
 package com.example.qrattendancesystem.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest.permission
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.qrattendancesystem.R
 
 class SplashScreen : AppCompatActivity() {
@@ -9,5 +12,23 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        checkPermissions()
+    }
+
+    private fun checkPermissions() {
+        // Check if the user gave the permission
+        if (ActivityCompat.checkSelfPermission(this, permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            goToMainActivity()
+        } else {
+            requestPermission()
+        }
+    }
+
+    private fun requestPermission() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun goToMainActivity() {
+
     }
 }
