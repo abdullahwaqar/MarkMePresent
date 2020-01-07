8a96a52089d8f7727c7338baa4aba01ab9cdee1e
awr417h <abdullahwaqar8@gmail.com>
2020-01-07 16:36:53 +0500

    init

---
+++ app/src/main/java/com/example/qrattendancesystem/ui/SplashScreen.kt
@@ -0,0 +1,13 @@
+package com.example.qrattendancesystem.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.qrattendancesystem.R
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
