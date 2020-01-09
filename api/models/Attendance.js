59cb968fe4b53fc7a6a7dce2b195d1f106ae452f
awr417h <abdullahwaqar8@gmail.com>
2020-01-09 19:49:35 +0500

    added readme and license

--- api/models/Attendance.js
+++ api/models/Attendance.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AttendanceSchema = new Schema({
-    class_id: { type: ObjectId }
-    
+    class_id: { type: ObjectId },
+    student_id: { type: ObjectId },
+    class_date_time: { type: Date, default: Date.now() }
 });
 
 module.exports = Attendance = mongoose.model('attendance', AttendanceSchema);
