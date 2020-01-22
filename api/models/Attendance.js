bfbbbf7d4ed789999c0343d3c174cc4f4207bf39
awr417h <abdullahwaqar8@gmail.com>
2020-01-22 07:05:06 +0500

    fixed schema issues

--- api/models/Attendance.js
+++ api/models/Attendance.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AttendanceSchema = new Schema({
-    class_id: { type: ObjectId },
+    class_id: { type: Schema.Types.ObjectId },
     student_id: { type: ObjectId },
     class_date_time: { type: Date, default: Date.now() }
 });
